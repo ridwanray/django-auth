a20556bb09099468c9c0a30d0468a4e4f9a6389c
ridwanray <alabarise@gmail.com>
2025-01-17 21:53:29 +0100

    update gitignore file

--- talent_base/settings.py
+++ talent_base/settings.py
@@ -22,7 +22,7 @@ from decouple import config
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bkib0rm^6toey7id=wap65#8)+bqp&#=q!l!(hkejf+ksdl581'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
